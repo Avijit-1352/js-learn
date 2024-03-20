d92acc48a6d0137e512c08be839ec4a21736198f
AVIJIT MONDAL <83465585+Avijit-1352@users.noreply.github.com>
2024-03-20 10:39:34 +0000

    Datatype

---
+++ basic/02_datatype.js
@@ -0,0 +1,40 @@
+"use strict";//treat all js code as newer version
+
+// alter(3+3)//We are using nodejs,not browser
+
+console.log(3
+    +3)//take care code readability
+console.log("jitur")    
+
+//Always refer website - t39 & MND
+
+/*DataType*/
+let name="sambhu"
+let age = 0
+let isloggedIn=false
+let state=null;
+
+//  Type of dataType:-
+/* 1. number=>range-2 to 53
+
+   2. Bigint(used for huge number like:stockmarket) 
+
+   3. string=>""
+
+   4. boolean =true/false
+
+   5. null=>stanalone value
+
+   6. undefined=>
+
+   7. symbol=>unique(used figma)
+
+   8. object    */
+
+console.log(typeof undefined);//Undefined
+console.log(typeof null);//Object
+// Note:-Important
+/* undefined datatype = undefined
+ null datatype=Object*/
+
+
