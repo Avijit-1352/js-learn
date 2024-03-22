6470ebbe0dc966b4b68da1aa2266357ddb6d4d69
AVIJIT MONDAL <83465585+Avijit-1352@users.noreply.github.com>
2024-03-22 13:14:04 +0000

    coversion operation

---
+++ basic/conversionOperation.js
@@ -0,0 +1,73 @@
+let score = "hitesh"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+score="33"
+score=true
+
+// conver any type to number Type
+// let valueNumber=Number(score);
+// console.log(valueNumber);
+//"33"=>33
+// "33abc"=NaN
+// true=>1;false=>0
+
+
+//convert boolean 
+let isLoggedId="string"
+
+let boolanConver=Boolean(isLoggedId);
+// console.log(boolanConver);
+
+// 1=>true;0=>false
+// ""=false
+// "string"=true
+
+// convert string
+let someNumber=33
+
+let stringNumber=String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//33=>"33"
+
+
+/***************Operations************/
+let value=3
+let negValue=-value
+// console.log(negValue); 
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(3%2);
+
+let str1="avi";
+let str2="jit";
+
+let str3=str1+str2;
+// console.log(str3);//avijit
+
+// console.log("2"+1);//21
+// console.log(2+"3");//23
+// console.log("1"+2+2);//122
+// console.log(2+"3"+1);//231
+// console.log(2+2+"4");//224
+
+console.log((3+4)*5%3);
+
+// console.log(+true);//1
+// console.log(+"");//0
+
+let num1,num2,num3;
+num1=num2=num3=2+2;
+// console.log(num1,num2,num3);//4 4 4
+
+let counter=54;
+// console.log(++counter);//55
+
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
