fa0b5a9db8a033ea22d75d7cc411a1fdd7be25e6
AVIJIT MONDAL <83465585+Avijit-1352@users.noreply.github.com>
2024-03-30 21:06:42 +0000

    object

---
+++ 02_basic/04_objects.js
@@ -0,0 +1,68 @@
+// const tinderUser = new Object();
+
+const tinderUser={}
+
+tinderUser.id="1234abc"
+tinderUser.name="lulu"
+tinderUser.isLoggedIn=false
+// console.log(tinderUser);// { id: '1234abc', name: 'lulu', isLoggedIn: false }
+
+const regularUser={
+    email:"some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"lulu",
+            lastname:"le"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1={1:"a",2:"b"}
+const obj2={1:"a",2:"b"}
+const obj4={1:"a",2:"b"}
+
+// const obj3={obj1,obj2}
+// console.log(obj3);//{ obj1: { '1': 'a', '2': 'b' }, obj2: { '1': 'a', '2': 'b' } }
+
+
+// const obj3=Object.assign({},obj1,obj2,obj4)
+// console.log(obj3)//{ '1': 'a', '2': 'b' }
+
+
+// const obj3={...obj1,...obj2}
+// console.log(obj3);//{ '1': 'a', '2': 'b' }
+
+const users=[
+    {
+        id:1,
+        email:"t@gmail.com"
+    },
+    {
+        id:1,
+        email:"t@gmail.com"
+    },
+    {
+        id:1,
+        email:"t@gmail.com"
+    }
+]
+
+
+users[1].email
+
+// console.log(Object.keys(tinderUser));//[ 'id', 'name', 'isLoggedIn' ]
+// console.log(Object.values(tinderUser));//[ '1234abc', 'lulu', false ]
+// console.log(Object.entries(tinderUser));//[ [ 'id', '1234abc' ], [ 'name', 'lulu' ], [ 'isLoggedIn', false ] ]
+
+const course={
+    cousrename:"js",
+    price:"999",
+    courseInstructor:"fvu"
+}
+
+// course.courseInstructor
+const {courseInstructor:instructor}=course
+// console.log(courseInstructor);
+console.log(instructor);
