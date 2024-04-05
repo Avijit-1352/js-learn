e530c6acbc6da5b417bad4bfbec99efcd316d8e0
AVIJIT MONDAL <83465585+Avijit-1352@users.noreply.github.com>
2024-04-05 16:01:43 +0000

    dom

---
+++ 06_DOM/one.js
@@ -0,0 +1,13 @@
+// window->document->HTML->head or body
+
+//All method of window
+//console.log(window)
+//console.dir(document)
+
+
+//target id
+// document.getElementById('firstHeading')
+
+
+//Change inner Text target Id
+//document.getElementById('firstHeading').innerHTML="<h1>Avijit Mondal</h1>"
